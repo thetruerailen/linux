e463d610962a2b00300a48038eaad9d4983687bd
Rae <103075692+thetruerailen@users.noreply.github.com>
2024-12-30 12:46:09 -0600

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,8 @@
+# Welcome to Debian
+
+# Step 1: Remove the # from the beginning of the BELOW line, so the code runs
+# npx  --yes gritty --port 8000 --command "bash ./ReplRoot/boot.sh" --auto-restart
+
+# Step 2: Delete the below line.
+node server
+
